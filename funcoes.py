124374752fe7099e4a9156b0bb985f5d64638358
mysterynd15 <marcosnadaijn@gmail.com>
2020-09-23 08:19:20 -0300

    Adicionado Funções de Leitura de CSV

--- funcoes.py
+++ funcoes.py
@@ -7,3 +7,32 @@ def trata_titulo (titulo_original):
     titulo = titulo.replace(' ', '')
     titulo = titulo.replace('-', '')
     return titulo
+
+
+#Tratamento do IP de Acesso
+
+def trata_ip (ip_acesso):
+    ip_acesso = str(ip_acesso)
+    ip_acesso = ip_acesso.replace("['", '')
+    ip_acesso = ip_acesso.replace("']", '')
+    ip_acesso = 'http://'+ip_acesso
+    return ip_acesso
+
+
+#Leitura CSV
+
+def ler_csv (nome_arquivo):
+    import csv
+
+    with open(nome_arquivo) as csv_file:
+        arquivo = csv.reader(csv_file)
+        lista = list(arquivo)
+
+    return lista
+
+
+#Contagem da Lista de IP's
+
+def cont_ip (lista):
+    tamanho = len(lista)
+    return tamanho
