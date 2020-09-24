f3d18cc7635c8fccff78b7442db9fe7b2de271ef
mysterynd15 <marcosnadaijn@gmail.com>
2020-09-24 09:40:48 -0300

    Script C60 Dividido em Funções

---
+++ c60_script_funcoes.py
@@ -0,0 +1,13 @@
+from c60_funcoes import *
+
+def c60_completo_quebrado_em_funcoes(browser):
+    c60_login(browser)
+    c60_botao_avancado(browser)
+    c60_botao_rede(browser)
+    c60_botao_DHCP(browser)
+    c60_DHCP(browser)
+    c60_wireless(browser)
+    c60_botao_sis_tools(browser)
+    c60_botao_ntp(browser)
+    c60_ntp(browser)
+    c60_logoff(browser)
