4b9574cacd23e4cf16a0d2b27f5947038efbe11d
mysterynd15 <marcosnadaijn@gmail.com>
2020-09-24 09:42:19 -0300

    Add-- "CRTL+A CRTL+DEL", Browser

--- funcoes.py
+++ funcoes.py
@@ -9,6 +9,7 @@ def trata_titulo (titulo_original):
     return titulo
 
 
+
 #Tratamento do IP de Acesso
 
 def trata_ip (ip_acesso):
@@ -17,6 +18,7 @@ def trata_ip (ip_acesso):
     ip_acesso = ip_acesso.replace("']", '')
     ip_acesso = 'http://'+ip_acesso
     return ip_acesso
+
 
 
 #Leitura CSV
@@ -31,8 +33,26 @@ def ler_csv (nome_arquivo):
     return lista
 
 
+
 #Contagem da Lista de IP's
 
 def cont_ip (lista):
     tamanho = len(lista)
     return tamanho
+
+
+
+# CRTL A + CRTL DEL
+
+def seleciona_deleta(campo):
+    from selenium.webdriver.common.keys import Keys
+    campo.send_keys(Keys.CONTROL + "a")
+    campo.send_keys(Keys.DELETE)
+
+
+
+# Browser
+
+def abre_navegador():
+    from selenium import webdriver
+    return webdriver.Chrome(executable_path="F:\Drivers\chromedriver.exe")
