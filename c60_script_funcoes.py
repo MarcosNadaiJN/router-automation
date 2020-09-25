b2e0cbb9b24ea56485a216c4eae4cf10beabb5c6
mysterynd15 <marcosnadaijn@gmail.com>
2020-09-25 09:09:53 -0300

    Reestruturado, implementado confirmação de login

--- c60_script_funcoes.py
+++ c60_script_funcoes.py
@@ -1,13 +1,35 @@
 from c60_funcoes import *
 
 def c60_completo_quebrado_em_funcoes(browser):
-    c60_login(browser)
-    c60_botao_avancado(browser)
-    c60_botao_rede(browser)
-    c60_botao_DHCP(browser)
-    c60_DHCP(browser)
-    c60_wireless(browser)
-    c60_botao_sis_tools(browser)
-    c60_botao_ntp(browser)
-    c60_ntp(browser)
-    c60_logoff(browser)
+    try:
+        c60_login()
+        sleep(1)
+        url_atual = confirma_login()
+        if 'index' in url_atual:
+            try:
+                c60_botao_avancado()
+                sleep(3)
+                c60_botao_rede()
+                sleep(1.5)
+                c60_botao_DHCP()
+                sleep(3)
+                c60_DHCP()
+            except:
+                print('Botão Avançado não localizado')
+            try:
+                c60_wireless()
+            except:
+                print('Não foi possivel configurar WIFI')
+            try:
+                c60_scroll_sidebarr()
+                sleep(0.5)
+                c60_botao_sis_tools()
+                sleep(1)
+                c60_botao_ntp()
+                c60_ntp()
+                c60_logoff()
+            except:
+                print('Não foi possivel Atualizar o NTP')
+                c60_logoff()
+    except:
+        print('Não foi possivel fazer login')
