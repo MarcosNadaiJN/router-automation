60050acc3f647436256cb1ee673e2c79643924ca
mysterynd15 <marcosnadaijn@gmail.com>
2020-09-25 09:08:39 -0300

    Corrigido BUGS, finalizar WIRELESS

--- c60_funcoes.py
+++ c60_funcoes.py
@@ -1,41 +1,39 @@
-#from c60_script import *
-#from selenium import webdriver
-#from selenium.common.exceptions import TimeoutException
 from time import sleep
-from selenium.webdriver.common.keys import Keys
+from definicoes_iniciais import *
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from definicoes_iniciais import *
+from selenium.webdriver.common.action_chains import ActionChains
 
-
-def c60_login(browser):
+def c60_login():
     entradas = browser.find_elements_by_tag_name('input')  # Localiza todos os inputs da pagina, e armazena na lista 'entradas'.
     campo_senha = entradas[20]  # O Campo da senha, corresponde ao item 20 da lista
     campo_senha.send_keys(senha)  # Digita a senha
     browser.find_element_by_id('login-btn').click()  # Clica no Botão de login
-    print('!--Login Efetuado com Sucesso--!')
 
-def c60_botao_avancado(browser):
+def confirma_login():
+    url = browser.current_url
+    return url
+
+def c60_botao_avancado():
     btn_avancado = WebDriverWait(browser, 8).until(EC.presence_of_element_located((By.ID, 'internet_status')))  #Espera Status da Internet Carregar
+    print('!--Login Efetuado com Sucesso--!')
     btn_avancado = browser.find_element_by_link_text('Avançado').click()  # Acessa aba Avançado
     print('Avançado')
 
-def c60_botao_rede(browser):
-    sleep(3)
+def c60_botao_rede():
     btn_rede = WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.ID, 'internet_ipv4_mac_address')))
     btn_rede = browser.find_element_by_link_text('Rede').click()  # Clica no Botão "Rede"
     print('Rede')
 
-def c60_botao_DHCP(browser):
+def c60_botao_DHCP():
     sleep(0.8)
     servidor_dhcp = WebDriverWait(browser, 1).until(EC.element_to_be_clickable((By.LINK_TEXT, 'Servidor DHCP')))
     servidor_dhcp = browser.find_element_by_link_text('Servidor DHCP').click()  # Clica no Botão "Servidor DHCP"
     print('Servidor DHCP')
 
-def c60_DHCP(browser):
+def c60_DHCP():
     from funcoes import seleciona_deleta
-    sleep(0.5)
     dns1 = browser.find_element_by_xpath('//*[@id="dns1"]')     #Localiza Campo DNS Primario
     seleciona_deleta(dns1)      #Limpa o Campo
     dns1.send_keys(dns_primario)        #Inseri Novo DNS Primario
@@ -43,7 +41,7 @@ def c60_DHCP(browser):
 
     dns2 = browser.find_element_by_xpath('//*[@id="dns2"]')     #Localiza Campo DNS Secundario
     seleciona_deleta(dns2)      #Limpa Campo
-    dns2.send_keys(Keys.DELETE)     #Inseri Novo DNS Secundario
+    dns2.send_keys(dns_secundario)     #Inseri Novo DNS Secundario
     print(f'DNS Secundario Atualizado para: {dns_secundario}')
 
     salvar_dns = browser.find_element_by_xpath('//*[@id="lan-ipv4-setting"]/div[8]/div/div/div/div[1]/button')  # Localiza Botão de Salvar DNS
@@ -51,22 +49,28 @@ def c60_DHCP(browser):
     sleep(0.3)
     print('DNS Atualizado com Sucesso')
 
-def c60_wireless(browser):
+def c60_wireless():
     wireless = browser.find_element_by_link_text('Wireless')  # Localiza Botão Wireless
     wireless.click()  # Clica no Botão Wireless
     print('Wireless')
 
-def c60_botao_sis_tools(browser):
+def c60_scroll_sidebarr():
+    fundo = browser.find_element_by_id('menu-advanced-system-tools-li')
+    action = ActionChains(browser)
+    action.move_to_element(fundo).perform()
+
+def c60_botao_sis_tools():
     sis_tools = browser.find_element_by_link_text('Ferramentas de Sistema').click()  # Localiza o "Ferramentas do Sistema" e Clica
     print('Ferramentas do Sistema')
 
-def c60_botao_ntp(browser):
-    ntp = WebDriverWait(browser, 2).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="menu-advanced-system-tools-li"]/div/ul/li[1]/a/span[2]')))
-    ntp = browser.find_element_by_xpath('//*[@id="menu-advanced-system-tools-li"]/div/ul/li[1]/a/span[2]').click  # Localiza o "Ajuste de Horario" e Clica
+def c60_botao_ntp():
+    #ntp = WebDriverWait(browser, 2).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="menu-advanced-system-tools-li"]/div/ul/li[1]/a/span[2]')))
+    ntp = browser.find_element_by_xpath('//*[@id="menu-advanced-system-tools-li"]/div/ul/li[1]/a/span[2]').click()     # Localiza o "Ajuste de Horario" e Clica
     print('Ajuste de Horario')
 
-def c60_ntp(browser):
+def c60_ntp():
     from funcoes import seleciona_deleta
+    espera = WebDriverWait(browser, 3).until(EC.element_to_be_clickable((By.ID, 'getGmt')))
     ntp_server_1 = browser.find_element_by_id('time_set_ntp1')      #Localiza Campo NTP1
     seleciona_deleta(ntp_server_1)      #Limpa Campo NTP 1
     ntp_server_1.send_keys(ntp_primario)      #Digita o Servidor NTP1 Atualizado
@@ -83,7 +87,7 @@ def c60_ntp(browser):
     print('NTP Atualizado com Sucesso')
     print('Configurações Finalizadas')
 
-def c60_logoff(browser):
+def c60_logoff():
     logoff = browser.find_element_by_id('top-control-logout').click()  # Localiza Botão de Logoff
     confirmacao_logoff = browser.find_element_by_xpath('/html/body/div[5]/div[2]/div[4]/div/div/div[2]/div/div[2]/button')  # Confirma Logoff
     confirmacao_logoff.click()  # Clica na Confirmação
