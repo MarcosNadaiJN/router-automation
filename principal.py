0182bc1c79894eca3713e8f3931a87bda08da120
mysterynd15 <marcosnadaijn@gmail.com>
2020-09-28 09:44:18 -0300

    Identifcação de Modelo passada para função

--- principal.py
+++ principal.py
@@ -1,14 +1,6 @@
-#1 - Alimentar IP's de Acesso Utilizando CSV FEITO
-#2 - Identificar o Modelo do Roteador Utilizando o Tittle FEITO
-#3 - Chamar o Arquivo Correspondente ao Modelo FEITO
-#4 - Marcar IP' como Visitado, e escrever se foi possivel acessar ou não
-#5 - Pegar Proximo Ip na Planilha e Reiniciar o Processo FEITO
-
-from wr849_script import *
-from time import sleep
-from funcoes import *
-from c60_script_funcoes import c60_completo_quebrado_em_funcoes
 import os
+from funcoes import *
+from time import sleep
 from definicoes_iniciais import *
 
 def main():
@@ -33,19 +25,12 @@ def main():
         titulo = browser.title      #Captura titulo da Pagina (Modelo do Roteador)
         titulo = trata_titulo(titulo)       #Trata String do Titulo
 
-        if titulo == 'archerc60':
-            c60_completo_quebrado_em_funcoes(browser)
-        elif titulo == 'tlwr849n':
-            wr849_completo(browser, senha, dns_primario, dns_secundario, ntp_primario, ntp_secundario)
-        else:
-            print('Modelo Não Compativel')
+        identifica_modelo(titulo)
 
         j += 1
         os.system('cls')
 
     print('LISTA FINALIZADA')
-
-
 
 
 if __name__ == '__main__':
