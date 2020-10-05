9da06d96f519a8558878c274740e6cb87d7c031c
mysterynd15 <marcosnadaijn@gmail.com>
2020-10-05 16:49:01 -0300

    Função "ler_senha" foi abstratida para "Ler_CSV"

--- definicoes_iniciais.py
+++ definicoes_iniciais.py
@@ -1,6 +1,6 @@
-from funcoes import abre_navegador, lista_senhas
+from funcoes import abre_navegador, ler_csv
 
-senha = lista_senhas()
+senha, tamanho = ler_csv('senhas.csv')
 
 dns_primario = '177.55.32.237'
 dns_secundario = '8.8.8.8'
