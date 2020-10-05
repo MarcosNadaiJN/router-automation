a2c918114383799fbc3ad9234269c13f3112add7
mysterynd15 <marcosnadaijn@gmail.com>
2020-10-05 16:49:24 -0300

    Add função "tentativas_login"

--- funcoes.py
+++ funcoes.py
@@ -8,25 +8,23 @@ def trata_titulo (titulo_original):
     return titulo
 
 
-
 #Tratamento do IP de Acesso
 def trata_lista (item_lista):
     item_lista = str(item_lista)
-    item_lista = item_lista.replace("['", '')
-    item_lista = item_lista.replace("']", '')
+    item_lista = item_lista.replace("'", '')
+    item_lista = item_lista.replace("'", '')
     return item_lista
-
 
 
 #Leitura CSV
 def ler_csv (nome_arquivo):
     import csv
-    with open(nome_arquivo) as csv_file:
-        arquivo = csv.reader(csv_file)
-        lista = list(arquivo)
-        tamanho = len(lista)
-    return lista, tamanho
-
+    lista = []
+    with open(nome_arquivo, newline='') as csv_file:
+        for linha in csv.reader(csv_file):
+            lista.append(linha[0])
+        tamanho_lista = len(lista)
+    return lista, tamanho_lista
 
 
 # CRTL A + CRTL DEL
@@ -39,7 +37,6 @@ def seleciona_deleta(campo):
     sleep(0.15)
 
 
-
 # Browser
 def abre_navegador():
     from selenium import webdriver
@@ -48,9 +45,9 @@ def abre_navegador():
 
 # Indentifica Modelo do Roteador e Roda o Script Correspondente
 def identifica_modelo(titulo):
-    from c60_script_funcoes import c60_completo_quebrado_em_funcoes
+    from script_por_roteador import c60_completo_dividido_em_funcoes
     if titulo == 'archerc60':
-        c60_completo_quebrado_em_funcoes(titulo)
+        c60_completo_dividido_em_funcoes(titulo)
     elif titulo == 'tlwr940n':
         print('TL-WR949N')
         #função do 949
@@ -58,25 +55,18 @@ def identifica_modelo(titulo):
         print('Modelo não localizado na Biblioteca')
 
 
-
-# Coleta a lista de senhas contida no arquivo CSV "senhas.csv"
-def lista_senhas():
-    import csv
-    with open('senhas.csv') as csv_file:
-        lista_senhas = csv.reader(csv_file)
-        lista_senhas = list(lista_senhas)
-        tamanho = len(lista_senhas)
-        lista_senhas = trata_lista(lista_senhas)
-        return lista_senhas, tamanho
-
-
-
 def tentativas_login(titulo):
     from c60_funcoes import c60_confirma_login, c60_login
-    lista, i = lista_senhas()
-    login_bem_sucedido = c60_confirma_login()
+    from time import sleep
+    lista, i = ler_csv('senhas.csv')
+    login_bem_sucedido = False
     j = 0
-    while j <= i or login_bem_sucedido == False:
+    while login_bem_sucedido == False:
         if titulo == 'archerc60':
-            c60_login(lista[j])
+            c60_login(trata_lista(lista[j]), j)
+        else:
+            print('Modelo não localizado')
         j += 1
+        sleep(1)
+        login_bem_sucedido = c60_confirma_login()
+    return login_bem_sucedido
