c172e47f44b9df8c9d6dd45bdc5e76dafe8340cf
mysterynd15 <marcosnadaijn@gmail.com>
2020-10-05 16:49:46 -0300

    Otimizado captura e Tratamento do Titulo da Pagina

--- principal.py
+++ principal.py
@@ -9,7 +9,7 @@ def main():
     browser.maximize_window()
 
     #Leitura do CSV com os IP's de Acesso
-    lista_ip, i = ler_csv(arquivo_csv)
+    lista_ip, i = ler_csv('testecsv.csv')
     print(f"Total de IP's Identificados no Arquivo: {i}")
 
     j = 0
@@ -18,11 +18,10 @@ def main():
         ip = trata_lista(lista_ip[j])
         print(f'Acessando: {ip}')
 
-        browser.get('http://'+ip)
-        sleep(3)
+        browser.get('http://'+ip)           #Acessa IP contido no CSV
+        sleep(2)
 
-        titulo = browser.title      #Captura titulo da Pagina (Modelo do Roteador)
-        titulo = trata_titulo(titulo)       #Trata String do Titulo
+        titulo = trata_titulo(browser.title)            #Captura titulo da Pagina (Modelo do Roteador) e Trata a String
 
         identifica_modelo(titulo)
 
