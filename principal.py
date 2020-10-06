357de57938fb20a66c0981cb791635c80586484c
mysterynd15 <marcosnadaijn@gmail.com>
2020-10-06 16:36:56 -0300

    Atualizado nome das funções

--- principal.py
+++ principal.py
@@ -1,4 +1,3 @@
-import os
 from funcoes import *
 from time import sleep
 from definicoes_iniciais import *
@@ -15,23 +14,15 @@ def main():
     while j <= tamanho_lista_ip-1:
 
         ip = trata_lista(lista_ip[j])
-        print(f'Acessando: {ip}')
 
-        browser.get('http://'+ip)           #Acessa IP contido no CSV
+        acessa_ip_porta(ip)
         sleep(2)
 
-        titulo = trata_titulo(browser.title)            #Captura titulo da Pagina (Modelo do Roteador) e Trata a String
+        titulo = trata_titulo(browser.title)    #Captura titulo da Pagina (Modelo do Roteador) e Trata a String
 
-        confimacao_login, DHCP, NTP = identifica_modelo(titulo)     #Configura Roteador e Retorna Flags para Cada Funcionalidade
-        '''
-            Necessario estudar uma forma,
-            Em que não é preciso passar todos estes valores como argumentos dentro das funções.
-            Porem o LOG do CSV está funcional.
-        '''
-        escrita_csv_log(confimacao_login, DHCP, NTP, j)     #Escreve em um CSV o LOG de quais operações foram realizadas
+        identifica_modelo(titulo, j)
 
         j += 1
-        os.system('cls')
 
     print('LISTA FINALIZADA')
 
