2c496d066fc1719087f613b0d3828fe3b8c3e946
mysterynd15 <marcosnadaijn@gmail.com>
2020-10-06 11:21:35 -0300

    Add função de escrita de LOG (Prototipo)

--- principal.py
+++ principal.py
@@ -9,11 +9,10 @@ def main():
     browser.maximize_window()
 
     #Leitura do CSV com os IP's de Acesso
-    lista_ip, i = ler_csv('testecsv.csv')
-    print(f"Total de IP's Identificados no Arquivo: {i}")
+    print(f"Total de IP's Identificados no Arquivo: {tamanho_lista_ip}")
 
     j = 0
-    while j <= i-1:
+    while j <= tamanho_lista_ip-1:
 
         ip = trata_lista(lista_ip[j])
         print(f'Acessando: {ip}')
@@ -23,7 +22,13 @@ def main():
 
         titulo = trata_titulo(browser.title)            #Captura titulo da Pagina (Modelo do Roteador) e Trata a String
 
-        identifica_modelo(titulo)
+        confimacao_login, DHCP, NTP = identifica_modelo(titulo)     #Configura Roteador e Retorna Flags para Cada Funcionalidade
+        '''
+            Necessario estudar uma forma,
+            Em que não é preciso passar todos estes valores como argumentos dentro das funções.
+            Porem o LOG do CSV está funcional.
+        '''
+        escrita_csv_log(confimacao_login, DHCP, NTP, j)     #Escreve em um CSV o LOG de quais operações foram realizadas
 
         j += 1
         os.system('cls')
