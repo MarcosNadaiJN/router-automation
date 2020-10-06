481b34a85b65f51a0f2d9acd68f1cc02a2d32c5c
mysterynd15 <marcosnadaijn@gmail.com>
2020-10-06 11:20:53 -0300

    Lista de IP's agora esta nas Definições iniciais

--- definicoes_iniciais.py
+++ definicoes_iniciais.py
@@ -1,6 +1,7 @@
 from funcoes import abre_navegador, ler_csv
 
 senha, tamanho = ler_csv('senhas.csv')
+lista_ip, tamanho_lista_ip = ler_csv('testecsv.csv')
 
 dns_primario = '177.55.32.237'
 dns_secundario = '8.8.8.8'
